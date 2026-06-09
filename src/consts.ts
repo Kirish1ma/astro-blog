6fd205312373eede0f12db45210ec65b1d748d02
Hatsumi <mail.sycamore587@passinbox.com>
2026-06-10 00:07:02 +0800

    refactor(site): reposition home and about with card navigation

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Kirishima Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "閱讀、札記與思考碎片的個人博客。";
